08e02cf66349c58cfa8d8c2773006f0fffbc3ecc
antony-kimanzi <antonykimanzi29@gmail.com>
2025-01-26 21:11:37 +0300

    edited app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_migrate import Migrate
-from models import db
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
+from models import TokenBlocklist, db
 
 app = Flask(__name__)
 
@@ -9,8 +11,20 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///trip_management.db"
 migrate = Migrate(app, db)
 db.init_app(app)
 
+app.config["JWT_SECRET_KEY"] = "abcdef$Xz!2@#1pQz*7d9_lR$32!89qweT^2aU!"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=7)
+jwt = JWTManager(app)
+jwt.init_app(app)
+
 from routes import *
 
 app.register_blueprint(user_bp)
 app.register_blueprint(trip_bp)
 app.register_blueprint(reservation_bp)
+
+@jwt.token_in_blocklist_loader
+def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
+    jti = jwt_payload["jti"]
+    token = db.session.query(TokenBlocklist.id).filter_by(jti=jti).scalar()
+
+    return token is not None
