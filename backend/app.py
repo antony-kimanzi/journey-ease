bf705a93c40500a882dfb2247d040b658e3c055a
antony-kimanzi <antonykimanzi29@gmail.com>
2025-01-26 15:56:26 +0300

    edited app.py

--- backend/app.py
+++ backend/app.py
@@ -9,3 +9,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///trip_management.db"
 migrate = Migrate(app, db)
 db.init_app(app)
 
+from routes import *
+
+app.register_blueprint("user_bp")
+app.register_blueprint("trip_bp")
+app.register_blueprint("reservation_bp")
